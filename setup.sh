f2cabe7bb0c68204dd2ce48943569ede7b5f6a0e
Pedro Ivo <pedryvo@gmail.com>
2017-12-05 00:18:46 -0300

    little funny message at shell

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose run web rails new . --force --database=postgresql && sudo chown -R $USER:$USER . && docker-compose build && rm config/database.yml && mv database.yml config/ && docker-compose up && docker-compose run web rake db:create && docker-compose up
+docker-compose run web rails new . --force --database=postgresql && sudo chown -R $USER:$USER . && docker-compose build && rm config/database.yml && mv database.yml config/ && docker-compose up && docker-compose run web rake db:create && echo "alright, now 'docker-compose up' this motherfucker"
